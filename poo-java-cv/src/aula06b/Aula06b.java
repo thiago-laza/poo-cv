9a9047d49dd9e4f548045b218881d809729a8d66
Thiago Cavalcanti <thiago.profmat@gmail.com>
2024-03-05 19:41:18 -0300

    Work: aula 06b - encapsulamento

---
+++ poo-java-cv/src/aula06b/Aula06b.java
@@ -0,0 +1,10 @@
+package aula06b;
+
+public class Aula06b {
+    public static void main(String[] args){
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.maisVolume();
+        c.abrirMenu();
+    }
+}
