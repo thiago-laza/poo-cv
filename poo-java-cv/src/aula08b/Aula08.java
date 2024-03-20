1819a375a8d484414dbf8eb92fefcdebb7cbb4a4
thiago-laza <thiago.profmat@gmail.com>
2024-03-19 22:07:53 -0300

    Home ex 8b

--- poo-java-cv/src/aula08b/Aula08.java
+++ poo-java-cv/src/aula08b/Aula08.java
@@ -5,31 +5,61 @@ public class Aula08 {
         int tamanhoLinha = 50;
         String linhaHifen = "-".repeat(tamanhoLinha);
         Lutador[] lutadores = new Lutador[6];
-        System.out.println(linhaHifen);
-        System.out.println("LUTADOR 1:");
+
+        //Instanciando a classe lutador
         lutadores[0] = new Lutador("Pretty Boy","Franca",31,1.75,68.9,11, 2,1);
+        lutadores[1] = new Lutador("Putscript","Brasil",29,1.68,57.8,14,2,3);
+        lutadores[2] = new Lutador("Snapshadow","EUA",35,1.65,80.9,12,2,1);
+        lutadores[3] = new Lutador("Dead Code","Australia",28,1.93,81.6,13,0,2);
+        lutadores[4] = new Lutador("Ufocobol","Brasil",37,1.70,119.3,5,4,3);
+        lutadores[5] = new Lutador("Nerdaard","EUA",30,1.81,105.7,12,2,4);
+
+        //Mostrando os lutadores
+        System.out.println(linhaHifen);
         lutadores[0].apresentar();
         System.out.println(linhaHifen);
-        System.out.println("LUTADOR 2:");
-        lutadores[1] = new Lutador("Putscript","Brasil",29,1.68,57.8,14,2,3);
         lutadores[1].apresentar();
         System.out.println(linhaHifen);
-        System.out.println("LUTADOR 3:");
-        lutadores[2] = new Lutador("Snapshadow","EUA",35,1.65,80.9,12,2,1);
         lutadores[2].apresentar();
         System.out.println(linhaHifen);
-        System.out.println("LUTADOR 4:");
-        lutadores[3] = new Lutador("Dead Code","Australia",28,1.93,81.6,13,0,2);
         lutadores[3].apresentar();
         System.out.println(linhaHifen);
-        System.out.println("LUTADOR 5:");
-        lutadores[4] = new Lutador("Ufocobol","Brasil",37,1.70,119.3,5,4,3);
         lutadores[4].apresentar();
         System.out.println(linhaHifen);
-        System.out.println("LUTADOR 6:");
-        lutadores[5] = new Lutador("Nerdaard","EUA",30,1.81,105.7,12,2,4);
         lutadores[5].apresentar();
+        System.out.println(linhaHifen);
 
+        System.out.println("Usando os metodos.");
+
+        //Usando os metodos ganharLuta, perderLuta e empatarLuta
+        lutadores[0].ganharLuta();
+        lutadores[1].empatarLuta();
+        lutadores[2].perderLuta();
+        lutadores[3].ganharLuta();
+        lutadores[4].empatarLuta();
+        lutadores[5].empatarLuta();
+
+        System.out.println(linhaHifen);
+        lutadores[0].apresentar();
+        System.out.println(linhaHifen);
+        lutadores[1].apresentar();
+        System.out.println(linhaHifen);
+        lutadores[2].apresentar();
+        System.out.println(linhaHifen);
+        lutadores[3].apresentar();
+        System.out.println(linhaHifen);
+        lutadores[4].apresentar();
+        System.out.println(linhaHifen);
+        lutadores[5].apresentar();
+        System.out.println(linhaHifen);
+
+        //Usando a classe luta (Agragacao)
+        System.out.println(linhaHifen);
+        Luta luta1 = new Luta();
+        luta1.marcarLuta(lutadores[0],lutadores[0]);
+        luta1.lutar();
+        lutadores[0].status();
+        lutadores[1].status();
 
 
     }
--- poo-java-cv/src/aula08b/Luta.java
+++ poo-java-cv/src/aula08b/Luta.java
@@ -1,6 +1,7 @@
 package aula08b;
 
-import aula07b.Lutador;
+
+import java.util.Random;
 
 public class Luta {
     //ATRIBUTOS
@@ -8,14 +9,48 @@ public class Luta {
     private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
-    //METODOS ESPECIAIS
-    public void marcarLuta(){
-
+    //Metodos publicos:
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria().equals(l2.getCategoria()) && l1 != l2){
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        }else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
     }
     public void lutar(){
-
+        if (aprovada){
+            System.out.println("DESAFIADO");
+            this.desafiado.apresentar();
+            System.out.println("DESAFIANTE");
+            this.desafiante.apresentar();
+            Random aleatorio = new Random();
+            int vencedor = aleatorio.nextInt(3);
+            switch (vencedor){
+                case 0://empate
+                    System.out.println("Empate");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1://desafiado vence
+                    System.out.println(this.desafiado.getNome() + " Venceu.");
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2://desafiante vence
+                    System.out.println(this.desafiante.getNome() + " Venceu.");
+                    this.desafiado.perderLuta();
+                    this.desafiante.ganharLuta();
+                    break;
+            }
+        }else {
+            System.out.println("A luta nao pode ocorrer.");
+        }
     }
-    //METODOS ESPECIAIS
+    //Metodos especiais
 
     public Lutador getDesafiado() {
         return desafiado;
@@ -24,25 +59,28 @@ public class Luta {
     public void setDesafiado(Lutador desafiado) {
         this.desafiado = desafiado;
     }
-    //METODOS
-    public void marcarLuta(Lutador l1, Lutador l2){
-        if (l1.getCategoria().equals(l2.getCategoria()) && !l1.equals(l2)){
-            aprovada = true;
-            desafiado = l1;
-            desafiante = l2;
-        }else {
-            aprovada = false;
-            desafiado = null;
-            desafiante = null;
-        }
+
+    public Lutador getDesafiante() {
+        return desafiante;
     }
 
-    public void lutar(boolean aprovada){
-        aprovada = true;
-       if (aprovada){
-            desafiado.apresentar();
-            desafiante.apresentar();
-            vencedor = 
-        }
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
     }
 }
